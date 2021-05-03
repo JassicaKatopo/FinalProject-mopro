52b1d62ebe5d7dc7484923d5ba395cbfdc04abb6
JassicaKatopo <s21810125@student.unklab.ac.id>
2021-05-03 22:21:43 +0800

    jesi create main project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Splashscreen from './pages/Spalshscreen'
+
+const App = () => {
+    return (
+        <Splashscreen/>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/pages/Spalshscreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Splashscreen = () => {
+    return (
+        <View>
+            <Text>splashscreen</Text>
+        </View>
+    )
+}
+
+export default Splashscreen
+
+const styles = StyleSheet.create({})
