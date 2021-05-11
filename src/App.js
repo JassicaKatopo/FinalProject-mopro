50208412dc7fdfcc099d1675cdb88959fbd40785
JassicaKatopo <s21810125@student.unklab.ac.id>
2021-05-11 15:10:16 +0800

    Jassica- fix error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import 'react-native-gesture-handler';
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import {Splashscreen, SignIn} from './pages'
->>>>>>> 3f70fbf906c3140b061247cf9ceba15d5ad3c803
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './router';
 
@@ -16,11 +10,5 @@ const App = () => {
         </NavigationContainer>
     );
 };
-<<<<<<< HEAD
-
-export default App;
-
-=======
->>>>>>> 3f70fbf906c3140b061247cf9ceba15d5ad3c803
 
 export default App;
